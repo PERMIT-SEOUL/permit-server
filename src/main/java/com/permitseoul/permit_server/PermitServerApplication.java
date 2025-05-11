d1473078312233706786e40018bd516cd1988eb1
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-05-11 17:42:38 +0900

    [setting] project 생성

---
+++ src/main/java/com/permitseoul/permit_server/PermitServerApplication.java
@@ -0,0 +1,13 @@
+package com.permitseoul.permit_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermitServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PermitServerApplication.class, args);
+	}
+
+}
