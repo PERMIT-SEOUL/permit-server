a8b97d76f863f16f9b007e8d24629654909b08a8
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-07-11 18:04:41 +0900

    feat: 예외 처리들 구현 - #34

---
+++ src/main/java/com/permitseoul/permitserver/domain/reservation/core/exception/ReservationCoreException.java
@@ -0,0 +1,6 @@
+package com.permitseoul.permitserver.domain.reservation.core.exception;
+
+import com.permitseoul.permitserver.domain.reservation.ReservationBaseException;
+
+public class ReservationCoreException extends ReservationBaseException {
+}
---
+++ src/main/java/com/permitseoul/permitserver/domain/reservation/core/exception/ReservationNotfoundException.java
@@ -0,0 +1,4 @@
+package com.permitseoul.permitserver.domain.reservation.core.exception;
+
+public class ReservationNotfoundException extends ReservationCoreException{
+}
