c5fff551a636e10dbb8a95e89df7fe54e0102bf6
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-07-14 02:54:28 +0900

    fix: 쿠키 도메인 변경

--- src/main/java/com/permitseoul/permitserver/domain/auth/api/controller/AuthController.java
+++ src/main/java/com/permitseoul/permitserver/domain/auth/api/controller/AuthController.java
@@ -29,8 +29,7 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<BaseResponse<?>> signUp(
             @RequestBody @Valid final SignUpRequest signUpRequest,
-            final HttpServletResponse response,
-            final HttpServletRequest request
+            final HttpServletResponse response
     ) {
         final TokenDto tokenDto = authService.signUp(
                 signUpRequest.userName(),
@@ -40,64 +39,50 @@ public class AuthController {
                 signUpRequest.socialType(),
                 signUpRequest.socialAccessToken()
         );
-        return responseWithGeneratedCookie(response, tokenDto, request);
+        return responseWithGeneratedCookie(response, tokenDto);
     }
 
     //로그인
     @PostMapping("/login")
     public ResponseEntity<BaseResponse<?>> login(
             @RequestBody @Valid final LoginRequest loginRequest,
-            final HttpServletResponse response,
-            final HttpServletRequest request
+            final HttpServletResponse response
     ) {
         final TokenDto tokenDto = authService.login(loginRequest.socialType(), loginRequest.authorizationCode(), loginRequest.redirectUrl());
-        return responseWithGeneratedCookie(response, tokenDto, request);
+        return responseWithGeneratedCookie(response, tokenDto);
     }
 
     //jwt 재발급
     @PostMapping("/reissue")
     public ResponseEntity<BaseResponse<?>> reissue(
             @CookieValue(name = Constants.REFRESH_TOKEN) Cookie refreshCookie,
-            final HttpServletResponse response,
-            final HttpServletRequest request
+            final HttpServletResponse response
     ) {
         final TokenDto tokenDto = authService.reissue(refreshCookie.getValue());
-        return responseWithGeneratedCookie(response, tokenDto, request);
+        return responseWithGeneratedCookie(response, tokenDto);
     }
-
-
-
+    
     //로그아웃
     @PostMapping("/logout")
     public ResponseEntity<BaseResponse<?>> logout(
             @UserId final Long userId,
             @CookieValue(name = Constants.REFRESH_TOKEN) final Cookie refreshTokenCookie,
-            final HttpServletResponse response,
-            final HttpServletRequest request
+            final HttpServletResponse response
             ) {
         authService.logout(userId, refreshTokenCookie.getValue());
 
-        String origin = request.getHeader("Origin");
-        if (origin == null) {
-            origin = "localhost";
-        }
         // 쿠키 삭제
-        final ResponseCookie deleteAccessToken = CookieCreatorUtil.deleteAccessTokenCookie(origin);
-        final ResponseCookie deleteRefreshToken = CookieCreatorUtil.deleteRefreshTokenCookie(origin);
+        final ResponseCookie deleteAccessToken = CookieCreatorUtil.deleteAccessTokenCookie();
+        final ResponseCookie deleteRefreshToken = CookieCreatorUtil.deleteRefreshTokenCookie();
         response.setHeader("Set-Cookie", deleteAccessToken.toString());
         response.addHeader("Set-Cookie", deleteRefreshToken.toString());
         return ApiResponseUtil.success(SuccessCode.OK);
     }
 
     private ResponseEntity<BaseResponse<?>> responseWithGeneratedCookie(final HttpServletResponse response,
-                                                                        final TokenDto tokenDto,
-                                                                        final HttpServletRequest request) {
-        String origin = request.getHeader("Origin");
-        if (origin == null) {
-            origin = "localhost";
-        }
-        final ResponseCookie accessTokenCookie = CookieCreatorUtil.createAccessTokenCookie(tokenDto.accessToken(), origin);
-        final ResponseCookie refreshTokenCookie = CookieCreatorUtil.createRefreshTokenCookie(tokenDto.refreshToken(), origin);
+                                                                        final TokenDto tokenDto) {
+        final ResponseCookie accessTokenCookie = CookieCreatorUtil.createAccessTokenCookie(tokenDto.accessToken());
+        final ResponseCookie refreshTokenCookie = CookieCreatorUtil.createRefreshTokenCookie(tokenDto.refreshToken());
         response.setHeader("Set-Cookie", accessTokenCookie.toString());
         response.addHeader("Set-Cookie", refreshTokenCookie.toString());
 
