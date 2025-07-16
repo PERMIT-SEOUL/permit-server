f93282e43d6dc4af845b216e67bab00af7b934b6
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-07-16 16:50:44 +0900

    fix: used 필드명 변경 - #49

--- src/main/java/com/permitseoul/permitserver/domain/coupon/core/repository/CouponRepository.java
+++ src/main/java/com/permitseoul/permitserver/domain/coupon/core/repository/CouponRepository.java
@@ -4,10 +4,9 @@ import com.permitseoul.permitserver.domain.coupon.core.domain.entity.CouponEntit
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface CouponRepository extends JpaRepository<CouponEntity, Long> {
     boolean existsByCouponCode(final String couponCode);
-    boolean existsByCouponCodeAndUsedFalse(String couponCode);
+    boolean existsByCouponCodeAndUsedFalse(final String couponCode);
 }
