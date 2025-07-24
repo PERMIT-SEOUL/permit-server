188c90529b20e0f488e93e5a1cfb8805469057bb
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-07-24 18:27:41 +0900

    chore: secureUrl 예외 변경 - #67

--- src/main/java/com/permitseoul/permitserver/global/util/SecureUrlUtil.java
+++ src/main/java/com/permitseoul/permitserver/global/util/SecureUrlUtil.java
@@ -1,5 +1,7 @@
 package com.permitseoul.permitserver.global.util;
 
+import com.permitseoul.permitserver.global.exception.UrlSecureException;
+import com.permitseoul.permitserver.global.response.code.ErrorCode;
 import org.hashids.Hashids;
 import org.springframework.stereotype.Component;
 
@@ -12,17 +14,17 @@ public class SecureUrlUtil {
         this.hashids = new Hashids(hashIdProperties.salt(), hashIdProperties.length());
     }
 
-    public String encode(Long id) {
+    public String encode(final Long id) {
         if (id == null) {
-            throw new IllegalArgumentException("ID must not be null");
+            throw new UrlSecureException(ErrorCode.INTERNAL_ID_ENCODE_ERROR);
         }
         return hashids.encode(id);
     }
 
-    public Long decode(String hash) {
-        long[] decoded = hashids.decode(hash);
+    public long decode(final String hash) {
+        final long[] decoded = hashids.decode(hash);
         if (decoded.length == 0) {
-            throw new IllegalArgumentException("Invalid Hash ID: " + hash);
+            throw new UrlSecureException(ErrorCode.BAD_REQUEST_ID_DECODE_ERROR);
         }
         return decoded[0];
     }
