54c7775a887e010322e1d54a1169eb6aa594e1c5
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-07-31 22:11:27 +0900

    chore: 메서드화 - #83

--- src/main/java/com/permitseoul/permitserver/domain/eventtimetable/timetable/api/service/TimetableService.java
+++ src/main/java/com/permitseoul/permitserver/domain/eventtimetable/timetable/api/service/TimetableService.java
@@ -24,6 +24,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.*;
 import java.util.stream.Collectors;
 
+
 @RequiredArgsConstructor
 @Service
 public class TimetableService {
@@ -36,60 +37,103 @@ public class TimetableService {
 
     @Transactional(readOnly = true)
     public TimetableResponse getEventTimetable(final long eventId, final Long userId) {
+        final Timetable timetable = getTimetableByEventId(eventId);
+        final List<TimetableArea> areaList = getAreaListByTimetableId(timetable.getTimetableId());
+        final List<TimetableCategory> categoryList = getCategoryListByTimetableId(timetable.getTimetableId());
+        final List<TimetableBlock> blockList = getBlockListByTimetableId(timetable.getTimetableId());
+
+        final Map<Long, String> categoryColorMap = mapCategoryColors(categoryList);
+        final Set<Long> likedBlockIds = getLikedBlockIdList(userId);
+
+        final List<TimetableResponse.Area> areaResponses = mapAreasToResponse(areaList);
+        final List<TimetableResponse.Block> blockResponses = mapBlocksToResponse(blockList, categoryColorMap, likedBlockIds);
+
+        return new TimetableResponse(
+                timetable.getStartDate(),
+                timetable.getEndDate(),
+                areaResponses,
+                blockResponses
+        );
+    }
+
+    private Timetable getTimetableByEventId(final long eventId) {
         try {
-            final Timetable timetable = timetableRetriever.getTimetableByEventId(eventId);
-            final List<TimetableArea> timetableAreaList = timetableAreaRetriever.findTimetableAreaListByTimetableId(timetable.getTimetableId());
-            final List<TimetableCategory> timetableCategoryList = timetableCategoryRetriever.findAllTimetableCategory(timetable.getTimetableId());
-            final List<TimetableBlock> timetableBlockList = timetableBlockRetriever.findAllTimetableBlockByTimetableId(timetable.getTimetableId());
-
-            final Map<Long, String> timetableCategoryColorMap = timetableCategoryList.stream()
-                    .collect(Collectors.toMap(
-                            TimetableCategory::getTimetableCategoryId,
-                            TimetableCategory::getCategoryColor)
-                    );
-
-            final Set<Long> likedBlockIds = (userId == null)
-                    ? Set.of()
-                    :new HashSet<>(timetableUserLikeRetriever.findAllBlockIdsLikedByUserId(userId));
-
-            final List<TimetableResponse.Area> areaResponses = timetableAreaList.stream()
-                    .sorted(Comparator.comparingInt(TimetableArea::getSequence))
-                    .map(area -> new TimetableResponse.Area(
-                            area.getTimetableAreaId(),
-                            area.getAreaName(),
-                            area.getSequence())
-                    ).toList();
-            final List<TimetableResponse.Block> blockResponses = timetableBlockList.stream()
-                    .sorted(Comparator.comparing(TimetableBlock::getStartDate))
-                    .map(timetableBlock -> {
-                        final String categoryColor = Optional.ofNullable(timetableCategoryColorMap.get(timetableBlock.getTimetableCategoryId()))
-                                .orElseThrow(() -> new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE_CATEGORY_COLOR));
-                        final String encodedTimetableBlockId = secureUrlUtil.encode(timetableBlock.getTimetableBlockId());
-
-                        return new TimetableResponse.Block(
-                                encodedTimetableBlockId,
-                                timetableBlock.getBlockName(),
-                                categoryColor,
-                                timetableBlock.getStartDate(),
-                                timetableBlock.getEndDate(),
-                                timetableBlock.getTimetableAreaId(),
-                                likedBlockIds.contains(timetableBlock.getTimetableBlockId()));
-                    }).toList();
-
-            return new TimetableResponse(
-                    timetable.getStartDate(),
-                    timetable.getEndDate(),
-                    areaResponses,
-                    blockResponses
-            );
+            return timetableRetriever.getTimetableByEventId(eventId);
         } catch (TimetableNotFoundException e) {
             throw new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE);
+        }
+    }
+
+    private List<TimetableArea> getAreaListByTimetableId(final long timetableId) {
+        try {
+            return timetableAreaRetriever.findTimetableAreaListByTimetableId(timetableId);
         } catch (TimetableAreaNotFoundException e) {
             throw new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE_AREA);
+        }
+    }
+
+    private List<TimetableCategory> getCategoryListByTimetableId(final long timetableId) {
+        try {
+            return timetableCategoryRetriever.findAllTimetableCategory(timetableId);
         } catch (TimetableCategoryNotfoundException e) {
             throw new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE_CATEGORY);
+        }
+    }
+
+    private List<TimetableBlock> getBlockListByTimetableId(final long timetableId) {
+        try {
+            return timetableBlockRetriever.findAllTimetableBlockByTimetableId(timetableId);
         } catch (TimetableBlockNotfoundException e) {
             throw new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE_BLOCK);
         }
     }
+
+    private Map<Long, String> mapCategoryColors(final List<TimetableCategory> categoryList) {
+        return categoryList.stream()
+                .collect(Collectors.toMap(
+                        TimetableCategory::getTimetableCategoryId,
+                        TimetableCategory::getCategoryColor
+                ));
+    }
+
+    private Set<Long> getLikedBlockIdList(final Long userId) {
+        if (userId == null) return Set.of();
+        return new HashSet<>(timetableUserLikeRetriever.findAllBlockIdsLikedByUserId(userId));
+    }
+
+    private List<TimetableResponse.Area> mapAreasToResponse(final List<TimetableArea> areaList) {
+        return areaList.stream()
+                .sorted(Comparator.comparingInt(TimetableArea::getSequence))
+                .map(area -> new TimetableResponse.Area(
+                        area.getTimetableAreaId(),
+                        area.getAreaName(),
+                        area.getSequence())
+                ).toList();
+    }
+
+    private List<TimetableResponse.Block> mapBlocksToResponse(
+            final List<TimetableBlock> blockList,
+            final Map<Long, String> categoryColorMap,
+            final Set<Long> likedBlockIds
+    ) {
+        return blockList.stream()
+                .sorted(Comparator.comparing(TimetableBlock::getStartDate))
+                .map(block -> {
+                    final String categoryColor = Optional.ofNullable(
+                                    categoryColorMap.get(block.getTimetableCategoryId()))
+                            .orElseThrow(() -> new NotfoundTimetableException(ErrorCode.NOT_FOUND_TIMETABLE_CATEGORY_COLOR));
+
+                    final String encodedBlockId = secureUrlUtil.encode(block.getTimetableBlockId());
+
+                    return new TimetableResponse.Block(
+                            encodedBlockId,
+                            block.getBlockName(),
+                            categoryColor,
+                            block.getStartDate(),
+                            block.getEndDate(),
+                            block.getTimetableAreaId(),
+                            likedBlockIds.contains(block.getTimetableBlockId()));
+                }).toList();
+    }
 }
+
