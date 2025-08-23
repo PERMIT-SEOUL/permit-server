c8913498a7c1e3daa33552b455675cffa1ec4d11
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-08-23 21:12:26 +0900

    feat: response dto 생성 - #91

--- src/main/java/com/permitseoul/permitserver/domain/admin/coupon/api/dto/response/CouponResponse.java
+++ src/main/java/com/permitseoul/permitserver/domain/admin/coupon/api/dto/response/CouponResponse.java
@@ -1,4 +1,26 @@
-package com.permitseoul.permitserver.domain.admin.coupon.api.dto.request;
+package com.permitseoul.permitserver.domain.admin.coupon.api.dto.response;
 
-public record CouponListResponse() {
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
+public record CouponResponse(
+        long couponId,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm")
+        LocalDateTime createDate,
+        int discountRate,
+        String couponCode,
+        String memo,
+        boolean isUsed
+) {
+    public static CouponResponse of(final long couponId,
+                                  final LocalDateTime createDate,
+                                  final int discountRate,
+                                  final String couponCode,
+                                  final String memo,
+                                  final boolean isUsed
+    ) {
+        return new CouponResponse(couponId, createDate, discountRate, couponCode, memo, isUsed);
+    }
 }
