c0e0366d39e370a7eaf9c1755e1701218ce8c8e7
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-09-09 01:45:02 +0900

    feat: ticketround saver 구현 - #117

---
+++ src/main/java/com/permitseoul/permitserver/domain/admin/ticketround/core/AdminTicketRoundSaver.java
@@ -0,0 +1,22 @@
+package com.permitseoul.permitserver.domain.admin.ticketround.core;
+
+import com.permitseoul.permitserver.domain.ticketround.core.domain.TicketRound;
+import com.permitseoul.permitserver.domain.ticketround.core.domain.entity.TicketRoundEntity;
+import com.permitseoul.permitserver.domain.ticketround.core.repository.TicketRoundRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class AdminTicketRoundSaver {
+    private final TicketRoundRepository ticketRoundRepository;
+
+    public TicketRound saveTicketRound(final long eventId,
+                                       final String ticketRoundTitle,
+                                       final LocalDateTime salesStartDate,
+                                       final LocalDateTime salesEndDate ) {
+        return TicketRound.fromEntity(TicketRoundEntity.create(eventId, ticketRoundTitle, salesStartDate, salesEndDate));
+    }
+}
