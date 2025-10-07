eeeb9a474747d4e28ca17a4b7faa5d7eebc9f470
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-10-07 12:41:40 +0900

    feat: asyncConfig 생성 - #153

---
+++ src/main/java/com/permitseoul/permitserver/global/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.permitseoul.permitserver.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "alertExecutor")
+    public Executor alertExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(4);   
+        executor.setQueueCapacity(20);
+        executor.setKeepAliveSeconds(60);
+        executor.setThreadNamePrefix("DiscordAlert-");
+        executor.setAllowCoreThreadTimeOut(true);
+        executor.initialize();
+
+        return executor;
+    }
+}
