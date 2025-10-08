0ca700f3b72729ad4f2f58bc2cfb04ad1dc4ade3
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-10-09 00:15:25 +0900

    feat: asyncConfig에서 mdc 비동기스레드 전파구현 - #153

--- src/main/java/com/permitseoul/permitserver/global/config/AsyncConfig.java
+++ src/main/java/com/permitseoul/permitserver/global/config/AsyncConfig.java
@@ -1,10 +1,12 @@
 package com.permitseoul.permitserver.global.config;
 
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -15,11 +17,26 @@ public class AsyncConfig {
     public Executor alertExecutor() {
         final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(2);
-        executor.setMaxPoolSize(4);   
+        executor.setMaxPoolSize(4);
         executor.setQueueCapacity(20);
         executor.setKeepAliveSeconds(60);
         executor.setThreadNamePrefix("DiscordAlert-");
         executor.setAllowCoreThreadTimeOut(true);
+
+        //AsyncAppender를 사용하는데, MDC를 비동기 스레드 전파하기 위함
+        executor.setTaskDecorator(runnable -> {
+            Map<String, String> contextMap = MDC.getCopyOfContextMap();
+            return () -> {
+                if (contextMap != null) {
+                    MDC.setContextMap(contextMap);
+                }
+                try {
+                    runnable.run();
+                } finally {
+                    MDC.clear();
+                }
+            };
+        });
         executor.initialize();
 
         return executor;
