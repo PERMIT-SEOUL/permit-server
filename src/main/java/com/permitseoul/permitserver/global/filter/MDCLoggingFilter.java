4f428f801ed87b0dd22946255e7f8fe4bb013f32
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-11-04 17:45:03 +0900

    feat: mdcFilter uri, status, method 추가 - #155

--- src/main/java/com/permitseoul/permitserver/global/filter/MDCLoggingFilter.java
+++ src/main/java/com/permitseoul/permitserver/global/filter/MDCLoggingFilter.java
@@ -16,35 +16,45 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.UUID;
 
-
 @Component
 @Slf4j
 @Profile("!local")
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class MDCLoggingFilter extends OncePerRequestFilter {
-    private final static String NGINX_REQUEST_ID = "X-Request-ID";
-    private final static String TRACE_ID = "trace_id";
+    private static final String NGINX_REQUEST_ID = "X-Request-ID";
+    private static final String TRACE_ID = "trace_id";
+
+    private static final String URI = "uri";
+    private static final String METHOD = "method";
+    private static final String STATUS = "status";
 
     @Override
     protected void doFilterInternal(@NonNull final HttpServletRequest request,
                                     @NonNull final HttpServletResponse response,
                                     @NonNull final FilterChain filterChain) throws ServletException, IOException {
         final String uri = request.getRequestURI();
+        // 헬스체크는 패스
         if (uri != null && uri.contains(Constants.HEALTH_CHECK_URL)) {
             filterChain.doFilter(request, response);
             return;
         }
-
         try {
             String traceId = request.getHeader(NGINX_REQUEST_ID);
             if (traceId == null || traceId.isBlank()) {
                 traceId = UUID.randomUUID().toString().replaceAll("-", "");
             }
             MDC.put(TRACE_ID, traceId);
+            MDC.put(URI, request.getRequestURI());
+            MDC.put(METHOD, request.getMethod());
 
             filterChain.doFilter(request, response);
+
+            MDC.put(STATUS, String.valueOf(response.getStatus()));
         } finally {
-            MDC.clear();
+            MDC.remove(STATUS);
+            MDC.remove(METHOD);
+            MDC.remove(URI);
+            MDC.remove(TRACE_ID);
         }
     }
 }
