fcf5a4c757712bd87eaaabaea617a10ac1382354
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-11-28 14:19:11 +0900

    feat: line color update strategy - #174

--- src/main/java/com/permitseoul/permitserver/domain/admin/timetable/category/core/strategy/impl/NotionTimetableCategoryLineColorUpdateStrategyImpl.java
+++ src/main/java/com/permitseoul/permitserver/domain/admin/timetable/category/core/strategy/impl/NotionTimetableCategoryLineColorUpdateStrategyImpl.java
@@ -1,4 +1,40 @@
 package com.permitseoul.permitserver.domain.admin.timetable.category.core.strategy.impl;
 
-public class NotionTimetableCategoryLineColorUpdateStrategyImpl {
+import com.permitseoul.permitserver.domain.admin.timetable.category.api.dto.NotionTimetableCategoryUpdateWebhookRequest;
+import com.permitseoul.permitserver.domain.admin.timetable.category.core.component.AdminTimetableCategoryRetriever;
+import com.permitseoul.permitserver.domain.admin.timetable.category.core.component.AdminTimetableCategoryUpdater;
+import com.permitseoul.permitserver.domain.admin.timetable.category.core.strategy.NotionTimetableCategoryUpdateWebhookStrategy;
+import com.permitseoul.permitserver.domain.admin.timetable.category.core.strategy.domain.NotionTimetableCategoryWebhookType;
+import com.permitseoul.permitserver.domain.eventtimetable.category.core.domain.entity.TimetableCategoryEntity;
+import com.permitseoul.permitserver.global.external.notion.exception.NotFoundNotionResponseException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotionTimetableCategoryLineColorUpdateStrategyImpl implements NotionTimetableCategoryUpdateWebhookStrategy {
+    private final AdminTimetableCategoryRetriever adminTimetableCategoryRetriever;
+    private final AdminTimetableCategoryUpdater adminTimetableCategoryUpdater;
+
+    private static final int NEW_LINE_COLOR_INDEX = 0;
+
+    @Override
+    public NotionTimetableCategoryWebhookType getType() {
+        return NotionTimetableCategoryWebhookType.LINE_COLOR;
+    }
+
+    @Override
+    public void updateNotionTimetableStageByNotionWebhook(final NotionTimetableCategoryUpdateWebhookRequest webhookRequest) {
+        final String rowId = webhookRequest.data().id();
+        final TimetableCategoryEntity categoryEntity = adminTimetableCategoryRetriever.findTimetableCategoryEntityByTimetableCategoryRowId(rowId);
+
+        final String newLineColor = webhookRequest.data().properties().lineColor().richText().get(NEW_LINE_COLOR_INDEX).plainText();
+        if (newLineColor == null || newLineColor.isEmpty()) {
+            throw new NotFoundNotionResponseException();
+        }
+
+        adminTimetableCategoryUpdater.updateTimetableLineColor(categoryEntity, newLineColor);
+    }
 }
