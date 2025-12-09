91dc9fed4efbedac2d7208c587623633931288d7
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-12-09 21:54:25 +0900

    fix: cookie max age 변경 - #193

--- src/main/java/com/permitseoul/permitserver/domain/auth/core/jwt/CookieCreatorUtil.java
+++ src/main/java/com/permitseoul/permitserver/domain/auth/core/jwt/CookieCreatorUtil.java
@@ -7,9 +7,7 @@ import org.springframework.http.ResponseCookie;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CookieCreatorUtil {
-    private static JwtProperties jwtProperties;
-    private static final long ACCESS_COOKIE_EXTRA_SECONDS = 5L * 60;   // 5분
-    private static final long REFRESH_COOKIE_EXTRA_SECONDS = 15L * 60; // 15분
+    private static final long COOKIE_MAX_AGE = 90L;   // 90초 테스트용
     private static final long RESERVED_MAX_AGE = 10L * 60; // 10분(10분간 선점 가능)
 
     public static ResponseCookie createReservationSessionCookie(final String sessionKey) {
@@ -22,10 +20,9 @@ public class CookieCreatorUtil {
                 .build();
     }
 
-    public static ResponseCookie createAccessTokenCookie(final String accessToken, final long accessTokenExpirationMillis) {
-        final long maxAgeSeconds = toCookieMaxAgeSeconds(accessTokenExpirationMillis, ACCESS_COOKIE_EXTRA_SECONDS);
+    public static ResponseCookie createAccessTokenCookie(final String accessToken) {
         return ResponseCookie.from(Constants.ACCESS_TOKEN, accessToken)
-                .maxAge(maxAgeSeconds)
+                .maxAge(COOKIE_MAX_AGE)
                 .path("/")
                 .httpOnly(true)
                 .secure(true)
@@ -33,10 +30,9 @@ public class CookieCreatorUtil {
                 .build();
     }
 
-    public static ResponseCookie createRefreshTokenCookie(final String refreshToken,  final long refreshTokenExpirationMillis) {
-        final long maxAgeSeconds = toCookieMaxAgeSeconds(refreshTokenExpirationMillis, REFRESH_COOKIE_EXTRA_SECONDS);
+    public static ResponseCookie createRefreshTokenCookie(final String refreshToken) {
         return ResponseCookie.from(Constants.REFRESH_TOKEN, refreshToken)
-                .maxAge(maxAgeSeconds)
+                .maxAge(COOKIE_MAX_AGE)
                 .path("/")
                 .httpOnly(true)
                 .secure(true)
@@ -64,8 +60,7 @@ public class CookieCreatorUtil {
                 .build();
     }
 
-    private static long toCookieMaxAgeSeconds(long jwtExpirationMillis, long extraSeconds) {
-        long baseSeconds = jwtExpirationMillis / 1000;
-        return baseSeconds + extraSeconds;
+    private static long toCookieMaxAgeSeconds(final long jwtExpirationMillis) {
+        return jwtExpirationMillis / 1000;
     }
 }
